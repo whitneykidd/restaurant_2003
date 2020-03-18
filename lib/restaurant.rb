e01e03f32c5ee994fc756e04a5ed672a0a536dc4
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-18 13:06:35 -0600

    Pass test_it_has_dishes

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant
-  attr_reader :opening_time, :name
+  attr_reader :opening_time, :name, :dishes
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
+    @dishes = []
   end
 end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -25,7 +25,7 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_dishes
-    skip
+
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
